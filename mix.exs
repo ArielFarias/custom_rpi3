65b914b55bbbf5f3e94cec3dc0f2bf34f29406e5
Ariel Farias <eu.ariel.farias@gmail.com>
2019-03-13 11:35:00 -0300

    try again artifact_sites

--- mix.exs
+++ mix.exs
@@ -34,8 +34,8 @@ defmodule CustomRpi3.MixProject do
   defp nerves_package do
     [
       type: :system,
-      artifact_url: [
-        " https://github.com/ArielFarias/custom_rpi3/releases/download/v1.6.3/custom_rpi3-portable-1.6.3.tar.gz"
+      artifact_sites: [
+        {:github_releases, "ArielFarias/#{@app}"},
       ],
       build_runner_opts: build_runner_opts(),
       platform: Nerves.System.BR,
