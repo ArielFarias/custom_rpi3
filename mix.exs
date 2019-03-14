b6990e5945559e7af15b046ef9934d7545a09c49
Ariel Farias <eu.ariel.farias@gmail.com>
2019-03-14 11:33:48 -0300

    Remove artifacts, add python to buildroot config, try to turn system partition r/w

--- mix.exs
+++ mix.exs
@@ -34,9 +34,9 @@ defmodule CustomRpi3.MixProject do
   defp nerves_package do
     [
       type: :system,
-      artifact_sites: [
-        {:github_releases, "ArielFarias/#{@app}"},
-      ],
+      # artifact_sites: [
+      #   {:github_releases, "ArielFarias/#{@app}"},
+      # ],
       build_runner_opts: build_runner_opts(),
       platform: Nerves.System.BR,
       platform_config: [
